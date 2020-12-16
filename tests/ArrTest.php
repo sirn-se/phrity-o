412840b6ab05cfb9c0b2730780a8af5ecd34c00c
Sören Jensen <sirn@sirn.se>
2020-12-16 23:14:55 +0100

    Php 8

--- tests/ArrTest.php
+++ tests/ArrTest.php
@@ -154,7 +154,7 @@ class ArrTest extends \PHPUnit\Framework\TestCase
     public function testUndefinedOffset(): void
     {
         $array = new Arr([1, 2, 3]);
-        $this->expectException('PHPUnit\Framework\Error\Notice');
+        $this->expectException('PHPUnit\Framework\Error\Error');
         $this->expectExceptionMessage('Undefined offset: 4');
         $array->offsetGet(4);
     }
